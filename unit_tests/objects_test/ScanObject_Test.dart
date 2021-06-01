12ad5ec509d771a65e65392ff725e020b3aa0571
Ezteras <L1ghtJ0k3yG1tHubJul1aL0g1n!>
2021-06-01 15:26:25 +0200

    Unit Test: ScanObject_Test Class

---
+++ unit_tests/objects_test/ScanObject_Test.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hrw_textscanner/objects/ScanObject.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class MockScanObject extends Mock implements ScanObject {}
+
+final String _testTitle = "testTitle";
+final String _testText = "Dies ist ein Text fuer den Test";
+final DateTime _testDateTime = DateTime.fromMillisecondsSinceEpoch(0);
+
+void main() {
+  group('Test correct Map creating in ScanObject class.', () {
+    test('Test if toMapForLocalDB creates correct Map.', () {
+      //given
+      ScanObject scanObject = ScanObject.fromJson(getValidLocalDbMap());
+      Map<String, dynamic> scanObjectToMap = scanObject.toMapForLocalDb();
+      Map<String, dynamic> expectedMap = getValidLocalDbMap();
+
+      //when
+      bool mapsEqual = mapEquals(scanObjectToMap, expectedMap);
+
+      //then
+      expect(mapsEqual, true);
+    });
+
+    test('Test if toMapForCloudDb creates correct Map.', () {
+      //given
+      Map<String, dynamic> mapForScanObjectInit = {
+        "title": _testTitle,
+        "textAsString": _testText,
+        "date": Timestamp.fromDate(_testDateTime)
+      };
+      ScanObject scanObject = ScanObject.fromJsonCloud(mapForScanObjectInit);
+      Map<String, dynamic> scanObjectToMap = scanObject.toMapForCloudDb();
+      Map<String, dynamic> expectedMap = getValidCloudDbMap();
+
+      //when
+      bool mapsEqual = mapEquals(scanObjectToMap, expectedMap);
+
+      //then
+      expect(mapsEqual, true);
+    });
+  });
+}
+
+Map<String, dynamic> getValidLocalDbMap() {
+  Map<String, dynamic> map = {
+    "title": _testTitle,
+    "textAsString": _testText,
+    "date": _testDateTime.millisecondsSinceEpoch
+  };
+  return map;
+}
+
+Map<String, dynamic> getValidCloudDbMap() {
+  Map<String, dynamic> map = {
+    "title": _testTitle,
+    "textAsString": _testText,
+    "date": _testDateTime.millisecondsSinceEpoch
+  };
+  return map;
+}
