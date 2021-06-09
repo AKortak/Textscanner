848fe1154fb8cfbe0127cceb827a359bffd86bbf
Ali Kortak <ali.kortak@hotmail.de>
2021-06-09 15:20:41 +0200

    changes for first ci/cd tool

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,5 +1,6 @@
 // Imports the Flutter Driver API.
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
@@ -15,10 +16,20 @@ void main() {
     final scannerButton = find.byValueKey('ScannerButton');
     final cloudSaveButtton = find.byValueKey('CloudButton');
     final localSaveButton = find.byValueKey('LocalButton');
+    final allowKey = find.text("text");
+
+    final zulassenbutton = find.text("Zulassen");
+    final ablehnenbutton = find.text("Ablehnen");
+
+    final textfield = find.byValueKey("TheValueKey");
 
     FlutterDriver driver;
 
     setUpAll(() async {
+      final Map<String, String> envVars = Platform.environment;
+      final String adbPath = envVars['ANDROID_HOME'] + '/platform-tools/adb.exe';
+      await Process.run(adbPath, ['shell', 'pm', 'grant', 'alikortak.hrw_textscanner', 'android.permission.READ_EXTERNAL_STORAGE']);
+      await Process.run(adbPath, ['shell', 'pm', 'grant', 'alikortak.hrw_textscanner', 'android.permission.WRITE_EXTERNAL_STORAGE']);
       driver = await FlutterDriver.connect();
       await driver.waitUntilFirstFrameRasterized();
     });
@@ -29,32 +40,48 @@ void main() {
       }
     });
 
-    test("login page test", () async {
-      driver.tap(emailTextfield);
-      driver.enterText("ali@hrw.de");
-      driver.tap(passwordTextfield);
-      driver.enterText("qwert123");
-      driver.tap(loginButton);
-      expect(await driver.getText(emailTextfield), "ali@hrw.de");
-      expect(await driver.getText(passwordTextfield), "qwert123");
-      driver.tap(loginButton);
-      sleep(Duration(seconds: 5));
+    // test("login page test", () async {
+    //   driver.tap(emailTextfield);
+    //   driver.enterText("ali@hrw.de");
+    //   driver.tap(passwordTextfield);
+    //   driver.enterText("qwert123");
+    //   driver.tap(loginButton);
+    //   expect(await driver.getText(emailTextfield), "ali@hrw.de");
+    //   expect(await driver.getText(passwordTextfield), "qwert123");
+    //   driver.tap(loginButton);
+    //   sleep(Duration(seconds: 5));
+    //
+    //   driver.tap(cloudSaveButtton);
+    //   sleep(Duration(seconds: 5));
+    //   driver.tap(localSaveButton);
+    //   sleep(Duration(seconds: 5));
+    //   driver.tap(settingsButton);
+    //   sleep(Duration(seconds: 2));
+    //   driver.tap(logoutButton);
+    //   sleep(Duration(seconds: 5));
+    // });
 
-
-      
-      driver.tap(cloudSaveButtton);
-      sleep(Duration(seconds: 5));
-      driver.tap(localSaveButton);
-      sleep(Duration(seconds: 5));
-      driver.tap(settingsButton);
-      sleep(Duration(seconds: 2));
-      driver.tap(logoutButton);
-      sleep(Duration(seconds: 5));
-    });
-
-    test('Photo recognitation', () async {
-      driver.tap(scannerButton);
-      sleep(Duration(seconds:5));
-    });
+    // test('Photo recognitation', () async {
+    //   driver.tap(emailTextfield);
+    //   driver.enterText("ali@hrw.de");
+    //   driver.tap(passwordTextfield);
+    //   driver.enterText("qwert123");
+    //   driver.tap(loginButton);
+    //   expect(await driver.getText(emailTextfield), "ali@hrw.de");
+    //   expect(await driver.getText(passwordTextfield), "qwert123");
+    //   driver.tap(loginButton);
+    //   sleep(Duration(seconds: 2));
+    //   driver.tap(scannerButton);
+    //   sleep(Duration(seconds: 2));
+    //   // driver.tap(find.text("DENY"));
+    //   // sleep(Duration(seconds: 2));
+    //
+    //   // sleep(Duration(seconds: 5));
+    //   //
+    //   // expect(await driver.getText(find.text("Zulassen")), "Zulassen");
+    //   //
+    //   // driver.tap(ablehnenbutton);
+    //   // sleep(Duration(seconds: 5));
+    // });
   });
 }
