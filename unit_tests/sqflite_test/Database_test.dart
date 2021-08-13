5a23f28a4236957f48468e54f6fbc2d8c3c3ea18
Ezteras <L1ghtJ0k3yG1tHubJul1aL0g1n!>
2021-08-13 16:48:41 +0200

    DDatabase_Test

--- unit_tests/sqflite_test/Database_test.dart
+++ unit_tests/sqflite_test/Database_test.dart
@@ -1,28 +1,23 @@
+import 'package:flutter/widgets.dart';
 import 'package:hrw_textscanner/sqflite/Database.dart';
 import 'package:mockito/mockito.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqlite_api.dart';
 import 'package:test/test.dart';
 
-class MockReturnDB extends Mock implements Future<Database> {}
-
-class MockDatabase extends Mock implements Database {}
-
-class MockSqlfite extends Mock implements Sqflite {}
-
-class DBForTest extends DB {}
-
-class MockDBForTest extends Mock implements DBForTest {}
-
 void main(List<String> args) {
-  Database database = MockDatabase();
-//  MockReturnDB returnedDB = MockReturnDB();
-  DBForTest db = DBForTest();
-//  MockDBForTest mockDB = MockDBForTest();
+  WidgetsFlutterBinding.ensureInitialized();
+  group('Test class Database', () {
+    test('Test Database Initialization', () async {
+      //init
+      await DB.init();
 
-  test('Test Database Initialisation', () async {
-    when(openDatabase(any)).thenAnswer((_) => database as Future<Database>);
-    // DB.init();
-    expect(database.isOpen, true);
+      //when
+      bool result = DB.db.isOpen;
+
+      //then
+      expect(result, true);
+      await DB.db.close();
+    });
   });
 }
