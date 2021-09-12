c2fd48fe1f0344cd436f722fd40e1e650ccdeb70
Fabio Pietryga <fabio.pietryga@stud.hs-ruhrwest.de>
2021-09-12 14:25:51 +0200

    adding build artefact

---
+++ build/app/generated/source/buildConfig/release/alikortak/hrw_textscanner/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package alikortak.hrw_textscanner;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "alikortak.hrw_textscanner";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
