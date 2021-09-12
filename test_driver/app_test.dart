e3889b72b735e59ab2d7ab9e46362a903b895fb0
Fabio Pietryga <fabio.pietryga@stud.hs-ruhrwest.de>
2021-09-12 14:31:31 +0200

    Updating test file

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,6 +1,5 @@
 // Imports the Flutter Driver API.
 import 'dart:io';
-import 'dart:math';
 
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
@@ -8,30 +7,33 @@ import 'package:test/test.dart';
 //flutter drive --target=test_driver/app.dart
 void main() {
   group('Scanner App', () {
-    final emailTextfield = find.byValueKey('Email');
-    final passwordTextfield = find.byValueKey('Password');
-    final loginButton = find.byValueKey('Login');
     final settingsButton = find.byValueKey('SettingsButton');
     final logoutButton = find.byValueKey('LogoutButton');
     final scannerButton = find.byValueKey('ScannerButton');
     final cloudSaveButtton = find.byValueKey('CloudButton');
     final localSaveButton = find.byValueKey('LocalButton');
-    final allowKey = find.text("text");
-
-    final zulassenbutton = find.text("Zulassen");
-    final ablehnenbutton = find.text("Ablehnen");
-
-    final textfield = find.byValueKey("TheValueKey");
 
     FlutterDriver driver;
 
     setUpAll(() async {
-      // final Map<String, String> envVars = Platform.environment;
-      // final String adbPath = envVars['ANDROID_HOME'] + '/platform-tools/adb.exe';
-      // await Process.run(adbPath, ['shell', 'pm', 'grant', 'alikortak.hrw_textscanner', 'android.permission.READ_EXTERNAL_STORAGE']);
-      // await Process.run(adbPath, ['shell', 'pm', 'grant', 'alikortak.hrw_textscanner', 'android.permission.WRITE_EXTERNAL_STORAGE']);
+      final String adbPath = 'C:/Users/aliko/AppData/Local/Android/Sdk/platform-tools/adb.exe';
+      await Process.run(adbPath, ['shell', 'pm', 'grant', 'alikortak.hrw_textscanner', 'android.permission.READ_EXTERNAL_STORAGE']);
+      await Process.run(adbPath, ['shell', 'pm', 'grant', 'alikortak.hrw_textscanner', 'android.permission.WRITE_EXTERNAL_STORAGE']);
       driver = await FlutterDriver.connect();
       await driver.waitUntilFirstFrameRasterized();
+      final emailTextfield = find.byValueKey('Email');
+      final passwordTextfield = find.byValueKey('Password');
+      final loginButton = find.byValueKey('Login');
+
+      driver.tap(emailTextfield);
+      sleep(Duration(seconds: 2));
+      driver.enterText("ali@hrw.de");
+      sleep(Duration(seconds: 2));
+      driver.tap(passwordTextfield);
+      sleep(Duration(seconds: 2));
+      driver.enterText("qwert123");
+      sleep(Duration(seconds: 2));
+      driver.tap(loginButton);
     });
 
     tearDownAll(() async {
@@ -40,7 +42,41 @@ void main() {
       }
     });
 
-    test("login page test", () async {
+    test("Check if saved data exists", () async {
+      sleep(Duration(seconds: 2));
+      final cloudButton = find.byValueKey('CloudButton');
+      driver.tap(cloudButton);
+      sleep(Duration(seconds: 4));
+      final cloudData = find.byValueKey('Cloud Data: 2');
+      driver.getText(cloudData).then((value) => expect(
+          value,
+          'Prewview: Text Message\n'
+          'Today 15:46Its Emma. I tried to call you but\n'
+          'signal bad. I been taken to\n'
+          'hospital after having a fall this\n'
+          'morning. If possible can you do\n'
+          'me a quick favour and text me x'));
+      sleep(Duration(seconds: 4));
+    });
+
+    test("Scan image", () async {
+      print("waited 2 Secunds");
+      sleep(Duration(seconds: 2));
+      final homeButton = find.byValueKey("Scanner");
+      driver.tap(homeButton);
+      sleep(Duration(seconds: 2));
+      final scannerButton = find.byValueKey("ScannerButton");
+      driver.tap(scannerButton);
+      sleep(Duration(seconds: 5));
+      final image = find.byValueKey("BackButton");
+      driver.tap(image);
+      sleep(Duration(seconds: 4));
+    });
+
+    test("test Premission", () async {
+      final emailTextfield = find.byValueKey('Email');
+      final passwordTextfield = find.byValueKey('Password');
+      final loginButton = find.byValueKey('Login');
       driver.tap(emailTextfield);
       driver.enterText("ali@hrw.de");
       driver.tap(passwordTextfield);
@@ -51,6 +87,23 @@ void main() {
       driver.tap(loginButton);
       sleep(Duration(seconds: 5));
 
+      driver.tap(scannerButton);
+      sleep(Duration(seconds: 2));
+    });
+
+    test("login page test", () async {
+      final emailTextfield = find.byValueKey('Email');
+      final passwordTextfield = find.byValueKey('Password');
+      final loginButton = find.byValueKey('Login');
+      driver.tap(emailTextfield);
+      driver.enterText("ali@hrw.de");
+      driver.tap(passwordTextfield);
+      driver.enterText("qwert123");
+      driver.tap(loginButton);
+      expect(await driver.getText(emailTextfield), "ali@hrw.de");
+      expect(await driver.getText(passwordTextfield), "qwert123");
+      driver.tap(loginButton);
+      sleep(Duration(seconds: 5));
       driver.tap(cloudSaveButtton);
       sleep(Duration(seconds: 5));
       driver.tap(localSaveButton);
@@ -61,27 +114,21 @@ void main() {
       sleep(Duration(seconds: 5));
     });
 
-    // test('Photo recognitation', () async {
-    //   driver.tap(emailTextfield);
-    //   driver.enterText("ali@hrw.de");
-    //   driver.tap(passwordTextfield);
-    //   driver.enterText("qwert123");
-    //   driver.tap(loginButton);
-    //   expect(await driver.getText(emailTextfield), "ali@hrw.de");
-    //   expect(await driver.getText(passwordTextfield), "qwert123");
-    //   driver.tap(loginButton);
-    //   sleep(Duration(seconds: 2));
-    //   driver.tap(scannerButton);
-    //   sleep(Duration(seconds: 2));
-    //   // driver.tap(find.text("DENY"));
-    //   // sleep(Duration(seconds: 2));
-    //
-    //   // sleep(Duration(seconds: 5));
-    //   //
-    //   // expect(await driver.getText(find.text("Zulassen")), "Zulassen");
-    //   //
-    //   // driver.tap(ablehnenbutton);
-    //   // sleep(Duration(seconds: 5));
-    // });
+    test('Photo recognitation', () async {
+      final emailTextfield = find.byValueKey('Email');
+      final passwordTextfield = find.byValueKey('Password');
+      final loginButton = find.byValueKey('Login');
+      driver.tap(emailTextfield);
+      driver.enterText("ali@hrw.de");
+      driver.tap(passwordTextfield);
+      driver.enterText("qwert123");
+      driver.tap(loginButton);
+      expect(await driver.getText(emailTextfield), "ali@hrw.de");
+      expect(await driver.getText(passwordTextfield), "qwert123");
+      driver.tap(loginButton);
+      sleep(Duration(seconds: 2));
+      driver.tap(scannerButton);
+      sleep(Duration(seconds: 2));
+    });
   });
 }
